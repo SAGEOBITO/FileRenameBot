5a3c348d4e4597e1e7463699dcf22673e016d275
SAGEOBITO <128952753+SAGEOBITO@users.noreply.github.com>
2023-10-24 17:42:11 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -25,3 +25,27 @@ async def see_caption(client, message):
        await message.reply_text(f"**𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽:-**\n\n`{caption}`")
     else:
        await message.reply_text("__**😔 𝚈𝙾𝚄 𝙳𝙾𝙽𝚃 𝙷𝙰𝚅𝙴 𝙰𝙽𝚈 𝙲𝙰𝙿𝙸𝙾𝙽**__")
+
+@Client.on_message(filters.private & filters.command('set_prefix'))
+async def add_caption(client, message):
+    if len(message.command) == 1:
+       return await message.reply_text("**__𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.__\n\n𝙴𝚡𝚊𝚖𝚙𝚕𝚎:- `/set_caption {prefix} -{filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
+    caption = message.text.split(" ", 1)[1]
+    await db.set_caption(message.from_user.id, caption=caption)
+    await message.reply_text("__**✅ YOUR PREFIX SUCCESSFULLY SAVED**__")
+
+@Client.on_message(filters.private & filters.command('del_prefix'))
+async def delete_caption(client, message):
+    caption = await db.get_caption(message.from_user.id)  
+    if not caption:
+       return await message.reply_text("__**😔 YOU DON'T HAVE ANY PREFIX**__")
+    await db.set_caption(message.from_user.id, caption=None)
+    await message.reply_text("__**❌️ YOUR PREFIX SUCCESSFILLY DELETED**__")
+
+@Client.on_message(filters.private & filters.command('see_prefix'))
+async def see_caption(client, message):
+    caption = await db.get_caption(message.from_user.id)  
+    if caption:
+       await message.reply_text(f"**𝚈𝙾𝚄𝚁 PREFIX:-**\n\n`{caption}`")
+    else:
+       await message.reply_text("__**😔 𝚈𝙾𝚄 𝙳𝙾𝙽𝚃 𝙷𝙰𝚅𝙴 𝙰𝙽𝚈 PREFIX**__")
